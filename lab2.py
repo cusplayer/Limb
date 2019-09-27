b2446aafe8d4508306ded210edafc3b7963e07b9
cusplayer <55591964+cusplayer@users.noreply.github.com>
2019-09-27 14:13:36 +0300

    Delete lab2.py

--- lab2.py
+++
@@ -1,9 +0,0 @@
-data = input("Enter anything. It'll sort. ")
-data  = data.split()
-import re 
-def sorted_nicely( l ): 
-    convert = lambda text: int(text) if text.isdigit() else text 
-    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
-    return sorted(l, key = alphanum_key)
-for x in sorted_nicely(data):
-    print(x)
